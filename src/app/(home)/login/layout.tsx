dca734b8c442f4c29c430e8dac21a1719e148b0f
phillip-steven <putu.phillip@gmail.com>
2024-05-31 12:11:02 +0800

    add some login page component

---
+++ src/app/(home)/login/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className='flex w-auto max-h-screen bg-neutral gap-x-0'>
+			<div className='flex-1 max-h-screen bg-base-100 overflow-y-auto overflow-x-auto'>
+				<div className='grid grid-rows-1'>{children}</div>
+			</div>
+		</div>
+	);
+}
